10651ee24cebe4864b4a3fcc0bcfdc38f9103f5c
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-10-06 07:37:24 +0530

    updating

---
+++ index.js
@@ -0,0 +1,30 @@
+var utils = require('utils');
+var serand = require('serand');
+
+exports.findOne = function (id, done) {
+    $.ajax({
+        method: 'GET',
+        url: exports.resolve('autos://apis/v/vehicle-makes/' + id),
+        dataType: 'json',
+        success: function (make) {
+            done(null, make);
+        },
+        error: function () {
+            done(new Error('error retrieving vehicle-makes ' + id));
+        }
+    });
+};
+
+exports.find = function (id, done) {
+    $.ajax({
+        method: 'GET',
+        url: exports.resolve('autos://apis/v/vehicle-makes'),
+        dataType: 'json',
+        success: function (makes) {
+            done(null, makes);
+        },
+        error: function () {
+            done(new Error('error retrieving vehicle-makes'));
+        }
+    });
+};
