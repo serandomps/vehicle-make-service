21d9c6022c70efdbf96bc2f4ed576aff5eb6cbd4
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-10-12 19:33:22 +0530

    updating

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var utils = require('utils');
 var serand = require('serand');
 
+var makes;
+
 exports.findOne = function (id, done) {
     $.ajax({
         method: 'GET',
@@ -16,12 +18,16 @@ exports.findOne = function (id, done) {
 };
 
 exports.find = function (done) {
+    if (makes) {
+        return done(null, makes);
+    }
     $.ajax({
         method: 'GET',
         url: utils.resolve('autos://apis/v/vehicle-makes'),
         dataType: 'json',
-        success: function (makes) {
-            done(null, makes);
+        success: function (data) {
+            makes = data;
+            done(null, data);
         },
         error: function () {
             done(new Error('error retrieving vehicle-makes'));
